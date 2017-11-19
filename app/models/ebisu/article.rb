f40ade4c2f3133974da03fc932c23f3a7c7c2b1c
Taichi Tomioka <mys.itachi@gmail.com>
2017-11-19 14:35:28 +0900

    Modify index page.

--- app/models/ebisu/article.rb
+++ app/models/ebisu/article.rb
@@ -1,6 +1,10 @@
 module Ebisu
   class Article < ApplicationRecord
-  	dragonfly_accessor :image
+    dragonfly_accessor :image
     has_many :paragraphs
+
+    def self.toparticles()
+      self.all.take(3)
+    end
   end
 end
