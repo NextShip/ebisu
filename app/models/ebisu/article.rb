415fab27c8ce3fc9b905f50ca3c2f98090b53dcc
Taichi Tomioka <mys.itachi@gmail.com>
2017-11-26 17:01:57 +0900

    Add factory methods to Ebisu::Article.

--- app/models/ebisu/article.rb
+++ app/models/ebisu/article.rb
@@ -12,5 +12,15 @@ module Ebisu
     def self.toparticles()
       self.all.take(3)
     end
+
+    def build_headline(params = {})
+      headline = Headline.new(content: params[:content])
+      paragraphs.build(type: "Ebisu::Paragraph::Headline", headline: headline, position: params[:position])
+    end
+
+    def build_body(params = {})
+      body = Body.new(content: params[:content])
+      paragraphs.build(type: "Ebisu::Paragraph::Body", body: body, position: params[:position])
+    end
   end
 end
