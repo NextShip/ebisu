1f2475cb41cf1e19ad8c5b512a3a3a47e84d8c48
Taichi Tomioka <mys.itachi@gmail.com>
2017-12-24 16:09:56 +0900

    Modify stylesheets.

--- app/decorators/ebisu/paragraph/body_decorator.rb
+++ app/decorators/ebisu/paragraph/body_decorator.rb
@@ -2,6 +2,6 @@ class Ebisu::Paragraph::BodyDecorator < Draper::Decorator
   delegate_all
 
   def content
-    h.content_tag(:p, object.content)
+    h.content_tag(:p, object.content, { class: "article__text" })
   end
 end
--- app/decorators/ebisu/paragraph/headline_decorator.rb
+++ app/decorators/ebisu/paragraph/headline_decorator.rb
@@ -2,6 +2,6 @@ class Ebisu::Paragraph::HeadlineDecorator < Draper::Decorator
   delegate_all
 
   def content
-    h.content_tag(:h2, object.content)
+    h.content_tag(:h2, object.content, { class: "article__headline" })
   end
 end
