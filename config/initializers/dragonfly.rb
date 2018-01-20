7913bcf449b929a3df69f386be085d437db77f70
Alex <akihito.tsuboi@gmail.com>
2018-01-20 21:30:15 +0900

    modify dragonfly

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,12 +4,22 @@ require 'dragonfly/s3_data_store'
 # Configure
 Dragonfly.app.configure do
   plugin :imagemagick
-
+  protect_from_dos_attacks true
   secret "f0f29ee054b86a6b2abcd526a057d8b1432bf2a89c7ba62caab4446b05de050a"
-
   url_format "/media/:job/:name"
 
-  datastore Ebisu.config.image_store_type, Ebisu.config.image_store_attributes
+  if Rails.env.development?
+    datastore :file,
+      root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+      server_root: Rails.root.join('public')
+  elsif Rails.env.production?
+    response_header "Cache-Control", "public, max-age=315360000"
+    datastore :s3,
+      bucket_name: ENV['EBISU_AWS_BUCKET_NAME'],
+      access_key_id: ENV['EBISU_AWS_ACCESSS_KEY_ID'], 
+      secret_access_key: ENV['EBISU_AWS_SECRET_ACCESS_KEY'],
+      region: ENV['EBISU_AWS_REGION']
+  end
 end
 
 # Logger
