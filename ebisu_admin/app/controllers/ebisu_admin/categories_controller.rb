7c6b5bcd24262ef27fb1955692152c211cf70a9d
Taichi Tomioka <mys.itachi@gmail.com>
2018-02-11 14:15:39 +0900

    Add editing Sort.

--- ebisu_admin/app/controllers/ebisu_admin/categories_controller.rb
+++ ebisu_admin/app/controllers/ebisu_admin/categories_controller.rb
@@ -3,7 +3,7 @@ require_dependency "ebisu_admin/application_controller"
 module EbisuAdmin
   class CategoriesController < ApplicationController
     def index
-      @categories = Ebisu::Category.all
+      @categories = Ebisu::Category::Sort.all
     end
 
     def show
@@ -21,6 +21,20 @@ module EbisuAdmin
       else
         flash.now[:alert] = @category.errors.full_messages.join(' ')
         render :new
+      end
+    end
+
+    def edit
+      @category = Ebisu::Category.find(params[:id])
+    end
+
+    def update
+      @category = Ebisu::Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to category_path(@category)
+      else
+        flash.now[:alert] = @category.errors.full_messages.join(' ')
+        render :edit
       end
     end
 
