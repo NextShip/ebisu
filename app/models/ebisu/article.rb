9984d8271d924406c78b5fb5b82ab34d9e6f9c53
Taichi Tomioka <mys.itachi@gmail.com>
2018-02-25 17:55:00 +0900

    Modify not to update published_at.

--- app/models/ebisu/article.rb
+++ app/models/ebisu/article.rb
@@ -1,8 +1,5 @@
 module Ebisu
   class Article < ApplicationRecord
-    # callbacks
-    before_save :set_publication_date
-
     # add-ons
     is_impressionable counter_cache: true
 
@@ -48,14 +45,6 @@ module Ebisu
     # instance methods
     def build_paragraph(params = {})
       paragraphs.build(type: params[:type], position: params[:position], delegate_attributes: { content: params[:content] })
-    end
-
-    private
-    def set_publication_date
-      if !is_published_was && is_published
-        self.published_at = Time.zone.now
-      end
-      self
     end
   end
 end
