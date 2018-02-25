b960144765284403e71e31db3ac6099b70d952f3
Taichi Tomioka <mys.itachi@gmail.com>
2018-02-25 16:29:22 +0900

    Add RSS feeder.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ebisu::Engine.routes.draw do
   get 'categories/show'
+  get 'feed', to: 'rss#index', defaults: { format: :rss }
 
   mount EbisuAdmin::Engine, at: "/ebisu_admin"
   resources :articles, only: [:index, :show]
