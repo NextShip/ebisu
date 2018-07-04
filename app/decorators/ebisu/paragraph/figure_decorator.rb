ae74c3883c85c8523724594a59b5213eaf83170d
Alex <akihito.tsuboi@gmail.com>
2018-07-04 19:20:48 +0900

    modify

--- app/decorators/ebisu/paragraph/figure_decorator.rb
+++ app/decorators/ebisu/paragraph/figure_decorator.rb
@@ -4,11 +4,13 @@ class Ebisu::Paragraph::FigureDecorator < Ebisu::ParagraphDecorator
   def content
     h.content_tag(:figure, {class: "article__figure"}) do
       h.concat h.content_tag(:img, '', {class: "article__image", src: object.content.url})
-      h.concat (
-        h.content_tag(:figcaption, {class: "article_figcaption"}) do
-          h.link_to("#{h.t('ebisu.activerecord.attributes.figure.source')}: #{object.source}", object.source_url)
-        end
-      )
+      if object.source_url.present?
+        h.concat (
+          h.content_tag(:figcaption, {class: "article_figcaption"}) do
+            h.link_to("#{h.t('ebisu.activerecord.attributes.figure.source')}: #{object.source}", object.source_url, rel: 'nofollow')
+          end
+        )
+      end
     end
   end
 end
