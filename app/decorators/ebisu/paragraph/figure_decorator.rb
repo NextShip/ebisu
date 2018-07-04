464cef796cba0702658e83b3670c4d8a01279299
Alex <akihito.tsuboi@gmail.com>
2018-07-04 19:58:47 +0900

    modify source_url of image

--- app/decorators/ebisu/paragraph/figure_decorator.rb
+++ app/decorators/ebisu/paragraph/figure_decorator.rb
@@ -4,7 +4,7 @@ class Ebisu::Paragraph::FigureDecorator < Ebisu::ParagraphDecorator
   def content
     h.content_tag(:figure, {class: "article__figure"}) do
       h.concat h.content_tag(:img, '', {class: "article__image", src: object.content.url})
-      if object.source_url.present?
+      if object.source_url.present? && object.source.present?
         h.concat (
           h.content_tag(:figcaption, {class: "article_figcaption"}) do
             h.link_to("#{h.t('ebisu.activerecord.attributes.figure.source')}: #{object.source}", object.source_url, rel: 'nofollow')
