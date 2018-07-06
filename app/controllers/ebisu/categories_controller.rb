eebe2bcf493c4878033627bc72dc156d24e7c278
Alex <akihito.tsuboi@gmail.com>
2018-07-06 19:24:26 +0900

    version up

--- app/controllers/ebisu/categories_controller.rb
+++ app/controllers/ebisu/categories_controller.rb
@@ -10,7 +10,7 @@ module Ebisu
       add_breadcrumb @category.parent.name, category_path(@category.parent) if @category.respond_to?(:parent)
       add_breadcrumb @category.name
 
-      set_meta_tags title: @category.name, description: @category.description
+      set_meta_tags title: "#{@category.name}に関する記事一覧", description: @category.description
     end
   end
 end
