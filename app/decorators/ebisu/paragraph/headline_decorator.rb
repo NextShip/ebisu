8fcb52aa6ff95c69840a74b80fd021d78448f43e
Taichi Tomioka <mys.itachi@gmail.com>
2018-07-22 14:29:08 +0900

    Add anchors to headilnes.

--- app/decorators/ebisu/paragraph/headline_decorator.rb
+++ app/decorators/ebisu/paragraph/headline_decorator.rb
@@ -2,6 +2,8 @@ class Ebisu::Paragraph::HeadlineDecorator < Ebisu::ParagraphDecorator
   delegate_all
 
   def content
-    h.content_tag(:h2, object.content, { class: "article__headline" })
+    h.content_tag(:h2, { class: "article__headline" }) do
+      h.content_tag(:a, object.content, { id: "ebisu_paragraph_headline_#{object.id}" })
+    end
   end
 end
--- app/decorators/ebisu/paragraph/subheadline_decorator.rb
+++ app/decorators/ebisu/paragraph/subheadline_decorator.rb
@@ -2,6 +2,8 @@ class Ebisu::Paragraph::SubheadlineDecorator < Ebisu::ParagraphDecorator
   delegate_all
 
   def content
-    h.content_tag(:h3, object.content, { class: "article__subheadline" })
+    h.content_tag(:h3, { class: "article__subheadline" }) do
+      h.content_tag(:a, object.content, { id: "ebisu_paragraph_subheadline_#{object.id}" })
+    end
   end
 end
