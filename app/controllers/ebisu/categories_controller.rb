59453db40d4e1604cb734aed7078694bdb791198
Alex <akihito.tsuboi@gmail.com>
2018-07-23 12:29:40 +0900

    hotfix ebisu

--- app/controllers/ebisu/categories_controller.rb
+++ app/controllers/ebisu/categories_controller.rb
@@ -7,7 +7,7 @@ module Ebisu
       @articles = Article.where(category_id: params[:id]).page(params[:page])
 
       add_breadcrumb "Top", articles_path
-      add_breadcrumb @category.parent.name, category_path(@category.parent) if @category.respond_to?(:parent)
+      add_breadcrumb @category.parent.name, category_path(@category.parent) if @category.respond_to?(:parent) && @category.parent.present?
       add_breadcrumb @category.name
 
       set_meta_tags title: "#{@category.name}に関する記事一覧", description: @category.description
