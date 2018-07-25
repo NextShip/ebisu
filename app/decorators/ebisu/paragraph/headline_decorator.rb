cb934e48e56453a803c8369e0d436be267e85e8d
Alex <akihito.tsuboi@gmail.com>
2018-07-25 17:31:09 +0900

    hotfix ebisu

--- app/decorators/ebisu/paragraph/headline_decorator.rb
+++ app/decorators/ebisu/paragraph/headline_decorator.rb
@@ -2,8 +2,6 @@ class Ebisu::Paragraph::HeadlineDecorator < Ebisu::ParagraphDecorator
   delegate_all
 
   def content
-    h.content_tag(:h2, { class: "article__headline" }) do
-      h.content_tag(:a, object.content, { id: "ebisu_paragraph_headline_#{object.id}" })
-    end
+    h.content_tag(:h2, object.content, { class: "article__headline", id: "ebisu_paragraph_headline_#{object.id}" })
   end
 end
