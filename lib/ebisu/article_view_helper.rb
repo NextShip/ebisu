12c06a65eaf34518aac45e4de2f933c0493d19c0
Alex <akihito.tsuboi@gmail.com>
2018-08-07 00:28:34 +0900

    delete unecessary

--- lib/ebisu/article_view_helper.rb
+++
@@ -1,7 +0,0 @@
-module Ebisu
-  module ArticleViewHelper
-    def render_article_panel(article)
-      render 'ebisu/articles/panel', article: article
-    end
-  end
-end
--- lib/ebisu/engine.rb
+++ lib/ebisu/engine.rb
@@ -1,13 +1,7 @@
-require 'ebisu/article_view_helper'
 require 'ebisu/config.rb'
 
 module Ebisu
   class Engine < ::Rails::Engine
     isolate_namespace Ebisu
-    initializer 'ebisu.action_view_helpers' do
-      ActiveSupport.on_load :action_view do
-        include Ebisu::ArticleViewHelper
-      end
-    end
   end
 end
