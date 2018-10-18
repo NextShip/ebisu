37db272db37f3e6f1a293e18a8d90a9364e7abbb
Alex <akihito.tsuboi@gmail.com>
2018-10-18 20:52:05 +0900

    update ebisu

--- app/decorators/ebisu/paragraph/figure_decorator.rb
+++ app/decorators/ebisu/paragraph/figure_decorator.rb
@@ -2,8 +2,8 @@ class Ebisu::Paragraph::FigureDecorator < Ebisu::ParagraphDecorator
   delegate_all
 
   def content
-    h.content_tag(:figure, {class: "article__figure"}) do
-      h.concat h.content_tag(:img, '', {class: "article__image", src: object.content&.thumb('780x405#')&.url})
+    h.content_tag(:figure, {class: "article__figure ebisu__article_figure"}) do
+      h.concat h.content_tag(:img, '', {class: "article__image ebisu__paragraph_image", src: object.content&.thumb('780x405#')&.url})
       if object.source_url.present? && object.source.present?
         h.concat (
           h.content_tag(:figcaption, {class: "article_figcaption"}) do
