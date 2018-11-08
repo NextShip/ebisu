99909e365c9327fb00514c1a3e30f431df5c25cb
Alex <akihito.tsuboi@gmail.com>
2018-11-09 01:10:58 +0900

    fix

--- lib/ebisu/engine.rb
+++ lib/ebisu/engine.rb
@@ -3,5 +3,8 @@ require 'ebisu/config.rb'
 module Ebisu
   class Engine < ::Rails::Engine
     isolate_namespace Ebisu
+    initializer "ebisu assets precompile" do |app|
+      app.config.assets.precompile += %w(ebisu/_custom.scss)
+    end
   end
 end
