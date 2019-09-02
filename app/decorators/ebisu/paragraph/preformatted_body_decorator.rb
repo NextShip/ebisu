db442f28c049a3b20ffac90602e80a7013785124
Alex <akihito.tsuboi@gmail.com>
2019-09-02 15:40:54 +0900

    modify preformatted body

--- app/decorators/ebisu/paragraph/preformatted_body_decorator.rb
+++ app/decorators/ebisu/paragraph/preformatted_body_decorator.rb
@@ -2,7 +2,8 @@ class Ebisu::Paragraph::PreformattedBodyDecorator < Ebisu::ParagraphDecorator
   delegate_all
 
   def content
-    h.content_tag(:p, object.content.gsub(/(\r\n|\r|\n)/, "<br />").html_safe, { class: "article__text" })
+
+    h.content_tag(:p, object.html_safe, { class: "article__text" })
   end
 
   def required_reading_time
